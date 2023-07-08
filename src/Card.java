bfbc1db895da6e5c25e65963895ac799cf62c09a
Mac Harvey <macharvey85@gmail.com>
2023-07-08 07:09:30 +0100

    Added Card object with basic functionality

---
+++ src/Card.java
@@ -0,0 +1,67 @@
+package src;
+
+public class Card 
+{
+
+    // instance variables 
+
+    private String face;
+    private int number;
+    private String value;
+
+
+    public Card()
+    {
+        this.value = "Joker";
+        this.face = "none";
+        this.number = 0;
+    }
+
+    public Card(String value, String face, int number)
+    {
+        this.value  = value;
+        this.face = face;
+        this.number = number;
+    }
+
+    // Accessors 
+
+    public String getFace()
+    {
+        return face;
+    }
+
+    public int getNumber()
+    {
+        return number;
+    }
+
+    public String getValue()
+    {
+        return value;
+    }
+
+    // Mutators
+
+    public void setFace(String newFace)
+    {
+        this.face = newFace;
+    }
+
+    public void setNumber(int newNumber)
+    {
+        this.number = newNumber;
+    }
+
+    public void setValue(String newValue)
+    {
+        this.value = newValue;
+    }
+
+    public String toString()
+    {
+        return value + " of " + face;
+    }
+
+
+}
